b0e949b93e442f716ea8ebd08720171318db48cc
nadia brandao silva <nadiabrandao98@gmail.com>
2025-03-25 22:09:11 -0300

    removendo vercel

--- backend/back-end-PI/server.js
+++ backend/back-end-PI/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // ✅ Permite que o Angular acesse o backend
 app.use(cors({
-    origin: ['http://localhost:4200', 'projeto-integrador2025.vercel.app'], // Adicione a URL da Vercel aqui
+    origin: ['http://localhost:4200', 'https://sage-kelpie-dad2a8.netlify.app'], // url front
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }));
